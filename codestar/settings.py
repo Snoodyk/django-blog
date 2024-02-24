80cc031e109ccc67a9f3c148166b0bb258cbb7b6
Nathan Snoodyk <nathansnoodyk@gmail.com>
2024-02-24 13:15:53 +0000

    added post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-m6asz5$f31p4bt88!xc0fh02@(2xo6#2m=_74xo&lkz1vs(9&)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 ALLOWED_HOSTS = ['8000-snoodyk-djangoblog-sk5hvdolpky.ws-eu108.gitpod.io', '.herokuapp.com']
